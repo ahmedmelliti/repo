54eaa41aa74874d3103e96690269d4372fe6248f
Ahmed Melliti <3407266@ppti-14-303-14.ufr-info-p6.jussieu.fr>
2018-02-26 18:02:12 +0100

    S6

--- module/tools.py
+++ module/tools.py
@@ -14,7 +14,7 @@ class Tools(object):
 			self.id_adverse_team = 1
 		else:
 			self.id_adverse_team = 2
-        	
+		
 		if self.id_team ==1:
 			self.cage = Vector2D(0,GAME_HEIGHT/2.)
 			self.cage_adv = Vector2D(GAME_WIDTH,GAME_HEIGHT/2.) 
@@ -25,8 +25,14 @@ class Tools(object):
 		self.ball_position = self.state.ball.position
 		self.ball_vitesse = self.state.ball.vitesse
 		self.p_position = self.state.player_state(id_team,id_player).position
+		self.vitesse = self.state.player_state(self.id_team,self.id_player).vitesse	
 		self.nb_players = state.nb_players(1)
 
+		if self.nb_players >= 2:
+			if self.id_player == 1:
+				self.ami_position = self.state.player_state(self.id_team,0).position
+			else:
+				self.ami_position = self.state.player_state(self.id_team,1).position
 
 	def goto(self, position):
 		return (position-self.state.player_state(self.id_team,self.id_player).position)
@@ -43,18 +49,24 @@ class Tools(object):
 	def canshoot(self):
 		return self.dbp()>=PLAYER_RADIUS+BALL_RADIUS
 
-	#def cage(self):
-	#	if self.id_team==1:
-	#		return Vector2D(GAME_WIDTH,GAME_HEIGHT/2.)
-	#	else:
-	#		return Vector2D(0,GAME_HEIGHT/2.)
+	def canshoot1(self):
+		if (self.p_position.distance(self.ball_position) < (PLAYER_RADIUS + BALL_RADIUS)):
+			return True
+		return False 
 
 	def adv_le_plus_proche(self):
-        	adv = 0
-        	for i in range(self.nb_players):
-            		if self.p_position.distance(self.state.player_state(self.id_adverse_team,i).position) < self.p_position.distance(self.state.player_state(self.id_adverse_team,adv).position):
-                		adv = i
-        	return self.state.player_state(self.id_adverse_team,adv).position
+		adv = 0
+		for i in range(self.nb_players):
+			if self.p_position.distance(self.state.player_state(self.id_adverse_team,i).position) < self.p_position.distance(self.state.player_state(self.id_adverse_team,adv).position):
+				adv = i
+		return self.state.player_state(self.id_adverse_team,adv).position
+
+	def adv_le_plus_proche_vitesse(self):
+		adv = 0
+		for i in range(self.nb_players):
+			if self.p_position.distance(self.state.player_state(self.id_adverse_team,i).position) < self.p_position.distance(self.state.player_state(self.id_adverse_team,adv).position):
+				adv = i
+		return self.state.player_state(self.id_adverse_team,adv).vitesse
 
 	def interception(self):
 		playergauche = 0
@@ -65,3 +77,35 @@ class Tools(object):
 			if self.state.player_state(self.id_adverse_team,i).position.x > self.state.player_state(self.id_adverse_team,playerdroit).position.x:
 				playerdroit = i
 			return (self.state.player_state(self.id_adverse_team,playerdroit).position + self.state.player_state(self.id_adverse_team,playergauche).position)/2
+
+	def joueur_position_shoot(self):
+		return self.p_position.distance(self.cage_adv)<GAME_WIDTH/3.
+	
+	def shoot1(self,p):
+		return SoccerAction(Vector2D(),p.normalize()*4.6)
+	
+	def shoot_cage(self):
+		return self.shoot1(self.cage_adv-self.ball_position)
+	
+	def dribble(self):
+		a = self.cage_adv-self.ball_position
+		a = a.normalize()*1.68
+		return SoccerAction(Vector2D(),a)
+
+	def passe(self):
+		return self.shoot1(self.ami_position - self.p_position)
+
+	def au_milieu(self):
+		return self.cage.distance(self.p_position)<GAME_WIDTH/2.
+
+	def au_cage(self):
+		return self.cage.distance(self.p_position)<GAME_WIDTH/4.
+
+	def en_attaque(self):
+		return self.cage.distance(self.p_position)<GAME_WIDTH*2/3.
+
+	def ball_avant_adv(self):
+		return self.p_position.distance(self.ball_position + self.vitesse) < self.p_position.distance(self.adv_le_plus_proche() + self.adv_le_plus_proche_vitesse())
+	
+	def ball_avant_adv_avc(self):
+		return self.p_position.distance(self.ball_position + self.vitesse) < self.p_position.distance(self.adv_le_plus_proche() + self.adv_le_plus_proche_vitesse())+20
--- simple_example.py
+++ simple_example.py
@@ -5,8 +5,10 @@ from module.strategies  import *
 ## Creation d'une equipe
 pyteam = SoccerTeam(name="PyTeam")
 thon = SoccerTeam(name="ThonTeam")
-pyteam.add("PyPlayer",RandomStrategy()) #Strategie qui ne fait rien
-thon.add("ThonPlayer",NewStrategy())   #Strategie aleatoire
+pyteam.add("PyPlayer",FonceurStrategy()) #Strategie qui ne fait rien
+thon.add("ThonPlayer",Defenseur())   #Strategie aleatoire
+pyteam.add("PyPlayer",RandomStrategy())
+thon.add("ThonPlayer",Milieu())
 
 #Creation d'une partie
 simu = Simulation(pyteam,thon)
